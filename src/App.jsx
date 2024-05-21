2ace10830cd739ecd1e27a626a51e7256a89128b
Fidel_Nava <eduardonaru@hotmail.com>
2024-05-20 20:52:27 -0600

    Diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,48 @@
 import { useState } from "react";
-import
+import 'style.css'; // Asegúrate de que la ruta sea correcta
 
 function App() {
-  const [n1,setN1]=useState(0);
-  const [n2,setN2]=useState(0);
-  const [resultado,setResultado]=useState(0);
+  const [n1, setN1] = useState(0);
+  const [n2, setN2] = useState(0);
+  const [resultado, setResultado] = useState(0);
 
-  const handleChange1=(event)=>{
+  const handleChange1 = (event) => {
     setN1(event.target.value);
   };
 
-  const handleChange2=(e)=>{
+  const handleChange2 = (e) => {
     setN2(e.target.value);
   };
 
-  const handleSumar = (event)=>{
+  const handleSumar = () => {
     setResultado(parseFloat(n1) + parseFloat(n2));
   };
 
-  const handleRestar = (event)=>{
+  const handleRestar = () => {
     setResultado(parseFloat(n1) - parseFloat(n2));
   };
 
-  const handleMulti = (event)=>{
+  const handleMulti = () => {
     setResultado(parseFloat(n1) * parseFloat(n2));
   };
 
-  const handleDividir = (event)=>{
+  const handleDividir = () => {
     setResultado(parseFloat(n1) / parseFloat(n2));
   };
-  
 
   return (
-    <>
+    <div className="calculator">
       <h1>CALCULADORA</h1>
-      
-      <input type="text" placeholder="Numero 1" value={n1} onChange={handleChange1}/>
-      <input type="text" placeholder="Numero 2" value={n2} onChange={handleChange2}/>
-      
-      <hr/>
-      
+      <input type="text" placeholder="Numero 1" value={n1} onChange={handleChange1} />
+      <input type="text" placeholder="Numero 2" value={n2} onChange={handleChange2} />
+      <hr />
       <button onClick={handleSumar}>+</button>
       <button onClick={handleRestar}>-</button>
       <button onClick={handleMulti}>x</button>
       <button onClick={handleDividir}>/</button>
-
-      <hr/>
-
+      <hr />
       <p>Resultado: {resultado}</p>
-    </>
+    </div>
   );
 }
 
